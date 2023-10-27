800812952e904134e1e3da4c344692db093ff0cd
Koizumi-Kento <s2271038@s.konan-u.ac.jp>
2023-10-27 17:54:46 +0900

    画面遷移の作成途中

--- app/src/main/java/com/example/prototypeapi22/MainActivity.java
+++ app/src/main/java/com/example/prototypeapi22/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.prototypeapi22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+ //スネークゲームへの画面遷移ボタン
+        Button snakeButton = (findViewById(R.id.SnakeButton));
+        snakeButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent snakeGame = new Intent(MainActivity.this,Snake_game.class);
+                startActivity(snakeGame);
+            }
+        });
+
+//数字ゲームへの画面遷移ボタン
+        Button numberButton = (findViewById(R.id.NumberButton));
+        numberButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent numberGame = new Intent(MainActivity.this,Speed_press.class);
+                startActivity(numberGame);
+            }
+        });
+
+//刹那ゲームへの画面遷移ボタン
+        Button setunaButton = (findViewById(R.id.SetunaButton));
+        setunaButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent setunaGame = new Intent(MainActivity.this,Setuna_game.class);
+                startActivity(setunaGame);
+            }
+        });
     }
 }
