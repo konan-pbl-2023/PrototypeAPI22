2118c3656e9df459d93c96591ee1abf19bf3be49
Koizumi-Kento <s2271038@s.konan-u.ac.jp>
2023-11-10 16:12:03 +0900

    シングルプレイ用の画面を追加いたした

---
+++ app/src/main/java/com/example/prototypeapi22/setuna_game_single.java
@@ -0,0 +1,14 @@
+package com.example.prototypeapi22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setuna_game_single extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setuna_game_single);
+    }
+}
