4862b0e306e6510499eb617cf16d41802c4caf38
osamumu <osamu110.prog@gmail.com>
2023-11-17 13:11:32 +0900

    snake_gameのtitleとresultの画面遷移処理を書いた

--- app/src/main/java/com/example/prototypeapi22/snake_result.java
+++ app/src/main/java/com/example/prototypeapi22/snake_result.java
@@ -2,7 +2,10 @@ package com.example.prototypeapi22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class snake_result extends AppCompatActivity {
 
@@ -14,7 +17,7 @@ public class snake_result extends AppCompatActivity {
         Button replay_btn = (findViewById(R.id.snake_replay_button));
         replay_btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Intent snakeTitle = new Intent(snake_title.this, snake_game.class);
+                Intent snakeTitle = new Intent(snake_result.this, snake_game.class);
                 startActivity(snakeTitle);
             }
         });
@@ -22,7 +25,7 @@ public class snake_result extends AppCompatActivity {
         Button return_btn = findViewById(R.id.snake_return_button);
         return_btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Intent snakeTitle = new Intent(snake_result.this, activity_main.class);
+                Intent snakeTitle = new Intent(snake_result.this, MainActivity.class);
                 startActivity(snakeTitle);
             }
         });
