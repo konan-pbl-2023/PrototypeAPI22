7a2a678c85036124f44087e38f133e038ce99161
osamummu <hoge@hoge.com>
2023-11-17 12:22:17 +0900

    title画面とresult画面の処理を書いた

---
+++ app/src/main/java/com/example/prototypeapi22/snake_result.java
@@ -0,0 +1,30 @@
+package com.example.prototypeapi22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class snake_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.snake_result);
+
+        Button replay_btn = (findViewById(R.id.snake_replay_button));
+        replay_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent snakeTitle = new Intent(snake_title.this, snake_game.class);
+                startActivity(snakeTitle);
+            }
+        });
+
+        Button return_btn = findViewById(R.id.snake_return_button);
+        return_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent snakeTitle = new Intent(snake_result.this, activity_main.class);
+                startActivity(snakeTitle);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/prototypeapi22/snake_title.java
@@ -0,0 +1,26 @@
+package com.example.prototypeapi22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class snake_title extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snake_title);
+
+        Button btn = findViewById(R.id.snake_play_button);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent snakeTitle = new Intent(snake_title.this, snake_game.class);
+                startActivity(snakeTitle);
+            }
+        });
+    }
+}
+
